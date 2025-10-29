905067702744c5c02ab66c71e945a6d03521bd15
Steven Tobares <steventobares65@gmail.com>
2025-10-29 17:38:41 -0600

    feat: create repository for job status history

---
+++ PrintingJobTracker.Infrastructure/Repository/Jobs/IJobStatusHistoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using PrintingJobTracker.Domain.Entities;
+
+namespace PrintingJobTracker.Infrastructure.Repository.Jobs
+{
+    public interface IJobStatusHistoryRepository
+    {
+        Task AddAsync(
+            string traceId,
+            JobStatusHistory history,
+            CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyList<JobStatusHistory>> GetJobStatusHistoriesPaginatedAsync(
+            Guid jobId,
+            int pageNumber,
+            int pageSize,
+            string traceId,
+            CancellationToken cancellationToken);
+    }
+}
