1496209539cf8c0220045629a13d44c5404ede7e
Steven Tobares <steventobares65@gmail.com>
2025-10-30 12:03:32 -0600

    feat(pages): add Jobs list, Job detail, Job history, and shared spinner component

--- PrintingJobTracker.Api/Program.cs
+++ PrintingJobTracker.Api/Program.cs
@@ -1,6 +1,5 @@
-using PrintingJobTracker.Api.Data;
 using PrintingJobTracker.Api.Extensions;
-using PrintingJobTracker.Application.Hubs;
+using PrintingJobTracker.Application;
 using PrintingJobTracker.Infrastructure;
 using PrintingJobTracker.Infrastructure.Services;
 
@@ -9,11 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSignalR();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 // Dependencys injection of Infrastructure project
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -21,21 +21,19 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 await app.ApplyMigration();
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-app.MapHub<AppHub>("/hub/app");
 
 _ = Task.Run(async () =>
 {
